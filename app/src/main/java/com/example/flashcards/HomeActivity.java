dba669de1c54215a7f01e66b798228488cdcce8c
Bmeyer-git <braxtonlmeyer@gmail.com>
2022-02-22 05:18:45 -0700

    Added functionality to change activities and a few testing buttons to allow in-app navigation.

--- app/src/main/java/com/example/flashcards/HomeActivity.java
+++ app/src/main/java/com/example/flashcards/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.flashcards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void launchMastery(View v){
+        // Switches to the mastery screen via a button
+
+        Intent i = new Intent(this, MasteryActivity.class);
+        startActivity(i);
+    }
+
+    public void launchPractice(View v){
+        // Switches to the practice screen via a button
+
+        Intent i = new Intent(this, PracticeActivity.class);
+        startActivity(i);
+    }
+
+    public void launchQuizzes(View v){
+        // Switches to the quiz screen via a button
+
+        Intent i = new Intent(this, QuizzesActivity.class);
+        startActivity(i);
     }
 }
